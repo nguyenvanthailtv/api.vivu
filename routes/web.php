15a561c2d96af59a269c200b3675dd341641b0bc
nguyen van thai <nguyenvanthailtv@gmail.com>
2024-05-15 21:29:42 +0700

    .api docs

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::prefix('activity')->group(function () {
-    Route::post('/', [ActivityController::class, 'create'])->name('activity.create');
-    Route::put('/{id}', [ActivityController::class, 'update'])->name('activity.update')->where('id', '[0-9]+');
-    Route::delete('/{id}', [ActivityController::class, 'delete'])->name('activity.delete')->where('id', '[0-9]+');
-    Route::get('/{id}', [ActivityController::class, 'find'])->name('activity.find')->where('id', '[0-9]+');
-    Route::get('/search', [ActivityController::class, 'search'])->name('activity.search');
-    Route::get('/all', [ActivityController::class, 'all'])->name('activity.all');
-    Route::post('/changeStatus', [ActivityController::class, 'changeStatus'])->name('activity.changeStatus');
-    Route::post('/multipleDelete', [ActivityController::class, 'multipleDelete'])->name('activity.multipleDelete');
-});
